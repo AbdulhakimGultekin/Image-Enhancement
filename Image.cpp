e10240a547c0737c4529a171f52fdec8117e36aa
AbdulhakimGultekin <hkmgltkn@gmail.com>
2018-04-20 21:46:45 +0300

    some blanks arranged

--- Image.cpp
+++ Image.cpp
@@ -10,149 +10,149 @@ using namespace std;
 
 // default constructor
 Image::Image(int nRows, int nCols){
-	if (nRows < 0 || nCols < 0){
-		cout << "Image: Index out of range.\n";
-		exit(1);
-	}
-	image = NULL;
-	nrows = nRows;
-	ncols = nCols;
-	maximum = 255;
-	if(nrows > 0 && ncols > 0)
-		createImage(nrows, ncols);
+    if (nRows < 0 || nCols < 0){
+        cout << "Image: Index out of range.\n";
+        exit(1);
+    }
+    image = NULL;
+    nrows = nRows;
+    ncols = nCols;
+    maximum = 255;
+    if(nrows > 0 && ncols > 0)
+        createImage(nrows, ncols);
 }
 
 // copy constructor
 Image::Image(const Image &img) {
-	image = NULL;
-	nrows = img.getRow();
-	ncols = img.getCol();
-	createImage(nrows, ncols);
+    image = NULL;
+    nrows = img.getRow();
+    ncols = img.getCol();
+    createImage(nrows, ncols);
 
-	for (int rows = 0; rows < nrows; rows++)
-		for (int cols = 0; cols < ncols; cols++)
-			image[rows * ncols + cols] = img(rows, cols);
+    for (int rows = 0; rows < nrows; rows++)
+        for (int cols = 0; cols < ncols; cols++)
+            image[rows * ncols + cols] = img(rows, cols);
 }
 
 // destructor
 Image::~Image(){
-	if(image)
-		delete [] image;
+    if(image)
+        delete [] image;
 }
 
 // Allocates memory and initialize the image.
 void Image::createImage(int nRows, int nCols){
-	if(image)
-		delete [] image;
-	nrows = nRows;
-	ncols = nCols;
-	image = (float *)new float[nrows * ncols];
-	if(!image){
-		cout << "CREATEIMAGE: Out of memory!";
-		exit(1);
-	}
-	initImage();			
+    if(image)
+        delete [] image;
+    nrows = nRows;
+    ncols = nCols;
+    image = (float *)new float[nrows * ncols];
+    if(!image){
+        cout << "CREATEIMAGE: Out of memory!";
+        exit(1);
+    }
+    initImage();			
 }
 
 // Initialize image.
 void Image::initImage(float initVal){
-	for(int numofPix = 0; numofPix < nrows * ncols; numofPix++)
-		image[numofPix] = initVal;
+    for(int numofPix = 0; numofPix < nrows * ncols; numofPix++)
+        image[numofPix] = initVal;
 }
 
 // Returns the number of rows.
 int Image::getRow() const {
-	return nrows;
+    return nrows;
 }
 
 // Returns the number of cols.
 int Image::getCol() const {
-	return ncols;
+    return ncols;
 }
 
 // Returns the maximum number.
 float Image::getMaximum() const {
-	float max = -10000;
-	for(int rows = 0; rows < nrows; rows++){
-		for(int cols = 0; cols < ncols; cols++){
-			if (max < image[rows * ncols + cols])
-				max = image[rows * ncols + cols];
-		}
-	}
-	return max;
+    float max = -10000;
+    for(int rows = 0; rows < nrows; rows++){
+        for(int cols = 0; cols < ncols; cols++){
+            if (max < image[rows * ncols + cols])
+                max = image[rows * ncols + cols];
+        }
+    }
+    return max;
 }
 
 // Returns the minimum number.
 float Image::getMinimum() const {
-	float min = 10000;
-	for(int rows = 0; rows < nrows; rows++){
-		for(int cols = 0; cols < ncols; cols++){
-			if (min > image[rows * ncols + cols])
-				min = image[rows * ncols + cols];
-		}
-	}
-	return min;
+    float min = 10000;
+    for(int rows = 0; rows < nrows; rows++){
+        for(int cols = 0; cols < ncols; cols++){
+            if (min > image[rows * ncols + cols])
+                min = image[rows * ncols + cols];
+        }
+    }
+    return min;
 }
 
 // Returns the pixel value.
 float Image::getPix(int Rows, int Cols){
-	return image[Rows * ncols + Cols];
+    return image[Rows * ncols + Cols];
 }
 
 // Returns the image using image buffer.
 Image Image::getImage() const {
-	Image temp;
-	temp.createImage(nrows, ncols);
-	for (int rows = 0; rows < nrows; rows++)
-		for (int cols = 0; cols < ncols; cols++)
-		temp(rows, cols) = image[rows * ncols + cols];
+    Image temp;
+    temp.createImage(nrows, ncols);
+    for (int rows = 0; rows < nrows; rows++)
+        for (int cols = 0; cols < ncols; cols++)
+            temp(rows, cols) = image[rows * ncols + cols];
 
-	return temp;
+    return temp;
 }
 
 // Sets the row number.
 void Image::setRow(int numberOfRows){
-	nrows = numberOfRows;
+    nrows = numberOfRows;
 }
 
 // Sets the column number.
 void Image::setCol(int numberOfColumns){
-	ncols = numberOfColumns;
+    ncols = numberOfColumns;
 }
 
 // Sets the pixel value.
 void Image::setPix(int rows, int cols, float value){
-	image[rows * ncols + cols] = value;
+    image[rows * ncols + cols] = value;
 }
 
 // Sets the image.
 void Image::setImage(Image &img){
-	int rows, cols;
+    int rows, cols;
 
-	for (rows = 0; rows < nrows; rows++)
-		for (cols = 0; cols < ncols; cols++)
-			image[rows * ncols + cols] = img(rows, cols);
+    for (rows = 0; rows < nrows; rows++)
+        for (cols = 0; cols < ncols; cols++)
+            image[rows * ncols + cols] = img(rows, cols);
 }
 
 float & Image::operator()(int rows, int cols) const {
-	return image[rows * ncols + cols];
+    return image[rows * ncols + cols];
 }
 
 const Image Image::operator=(const Image& img) {
-	int rows, cols;
+    int rows, cols;
 
-	if (this == &img)
-		return *this;
+    if (this == &img)
+        return *this;
 
-	nrows = img.getRow();
-	ncols = img.getCol();
-	createImage(nrows, ncols);
+    nrows = img.getRow();
+    ncols = img.getCol();
+    createImage(nrows, ncols);
 
-	for (rows = 0; rows < nrows; rows++)
+    for (rows = 0; rows < nrows; rows++)
 		for (cols = 0; cols < ncols; cols++)
-			(*this)(rows, cols) = img(rows, cols);
+            (*this)(rows, cols) = img(rows, cols);
 	
-	return *this;
+    return *this;
 }
 
 /*
@@ -222,12 +222,12 @@ Read image from a file.
     ifp.read((char *)img, (nRows * nCols * sizeof(unsigned char)));
 
     for (rows = 0; rows < nRows; rows++)
-      for (cols = 0; cols < nCols; cols++)
-          image[rows * nCols + cols] = (float) img[rows * nCols + cols];
+        for (cols = 0; cols < nCols; cols++)
+            image[rows * nCols + cols] = (float) img[rows * nCols + cols];
 
-	ifp.close();
+    ifp.close();
 
-	delete [] img;
+    delete [] img;
 }
 
 
@@ -266,24 +266,24 @@ void Image::writeImage(char *fname, bool flag) {
 
 
     for (i = 0; i< nrows; i++){
-      for (j = 0; j < ncols; j++){
-		// rescale if the flag is set
-		if ((maxi != mini) && flag == true)
-			img[i * ncols + j] = (unsigned char)  ((image[i * ncols + j]-mini)/(float)(maxi-mini)*255.0);
-		// any intensity that is larger than the maximum would be set as maximum
-		else if (image[i * ncols + j] > 255)
-			img[i * ncols + j] = 255;
-		else if (image[i * ncols + j] < 0)
-			img[i * ncols + j] = 0;
-		else
+        for (j = 0; j < ncols; j++){
+            // rescale if the flag is set
+		    if ((maxi != mini) && flag == true)
+			    img[i * ncols + j] = (unsigned char)  ((image[i * ncols + j]-mini)/(float)(maxi-mini)*255.0);
+		    // any intensity that is larger than the maximum would be set as maximum
+		    else if (image[i * ncols + j] > 255)
+			    img[i * ncols + j] = 255;
+		    else if (image[i * ncols + j] < 0)
+			    img[i * ncols + j] = 0;
+		    else
 			img[i * ncols + j] = (unsigned char)  image[i * ncols + j];
-      }
-	}
+        }
+    }
 	
     ofp.write((char *)img, (nrows * ncols * sizeof(unsigned char)));
 
-	ofp.close();
-	delete [] img;
+    ofp.close();
+    delete [] img;
 }
 
 // MEMBER FUNCTIONS FOR IMAGE ENHANCEMENT
@@ -304,15 +304,15 @@ Image Image::thresholdImage(float thresholdValue, float lowValue, float highValu
 
 // "negativeImage" function makes the image negatived.
 Image Image::negativeImage(){
-	int L = 256;
-	Image temp;
+    int L = 256;
+    Image temp;
     temp.createImage(nrows, ncols);
-	// So as to get negative of an image used method is s = L - 1 - r.
+    // So as to get negative of an image used method is s = L - 1 - r.
     for (int rows = 0; rows < nrows; rows++)
         for (int cols = 0; cols < ncols; cols++)
             temp.setPix(rows, cols, L - 1 - image[rows * ncols + cols]);  // it's a way to set
 
-	return temp;
+    return temp;
 }
 // "smoothImage" function can be used to blur any given image by using dimXdim average filter.
 Image Image::smoothImage(int dim){
@@ -323,24 +323,24 @@ Image Image::smoothImage(int dim){
 	float var;
 
 	// Blurring the image with an averaging mask by regarding neighborhood specifications.
-	for (int rows = 0; rows < nrows; rows++)
+	for (int rows = 0; rows < nrows; rows++){
         for (int cols = 0; cols < ncols; cols++){
-        	for (int i = -1 * ((dim - 1) / 2); i < ((dim - 1) / 2 + 1); i++){
-        		for(int k = -1 * ((dim - 1) / 2); k < ((dim - 1) / 2 + 1); k++){
+            for (int i = -1 * ((dim - 1) / 2); i < ((dim - 1) / 2 + 1); i++){
+                for(int k = -1 * ((dim - 1) / 2); k < ((dim - 1) / 2 + 1); k++){
         			if((rows + i < 0) || (cols + k < 0) || (rows + i > nrows - 1) || (cols + k > ncols - 1))
         				var = 0;
         			else
         				var = this->getPix(rows + i, cols + k);
 
                     temp_val = temp_val + var;
-        		}
-        	}
+        	    }
+            }
 			// Result is the average value for every pixels (x,y).
             result = (1.0 / (dim * dim)) * temp_val;
             temp.setPix(rows, cols, result);
             temp_val = 0;
         }
-
+    }
     return temp;
 }
 
@@ -352,30 +352,31 @@ Image Image::Gaussian(int dim){
 	float temp_val = 0;
 	float var;
 	float sigma = 3.0;
-	float result;
+    float result;
 
-	// Generating Gaussian spatial filter
-	for (int rows = 0; rows < h.getRow(); rows++)
+    // Generating Gaussian spatial filter
+    for (int rows = 0; rows < h.getRow(); rows++)
         for (int cols = 0; cols < h.getCol(); cols++){
             h.setPix(rows, cols, exp(-1 * (rows * rows + cols * cols) / (2 * sigma * sigma)));
         }
 
-	for (int rows = 0; rows < nrows; rows++){
+    for (int rows = 0; rows < nrows; rows++){
         for (int cols = 0; cols < ncols; cols++){
-        	for (int i = -1 * ((dim - 1) / 2); i < ((dim - 1) / 2 + 1); i++){
-        		for(int k = -1 * ((dim - 1) / 2); k < ((dim - 1) / 2 + 1); k++){
-        			if((rows + i < 0) || (cols + k < 0) || (rows + i > nrows - 1) || (cols + k > ncols - 1))
-        				var = 0;
-        			else
-        				var = this->getPix(rows + i,cols + k) * h.getPix(i + ((dim - 1) / 2), k + ((dim - 1) / 2));
+            for (int i = -1 * ((dim - 1) / 2); i < ((dim - 1) / 2 + 1); i++){
+        	    for(int k = -1 * ((dim - 1) / 2); k < ((dim - 1) / 2 + 1); k++){
+        		    if((rows + i < 0) || (cols + k < 0) || (rows + i > nrows - 1) || (cols + k > ncols - 1))
+        			    var = 0;
+                    else
+        			    var = this->getPix(rows + i,cols + k) * h.getPix(i + ((dim - 1) / 2), k + ((dim - 1) / 2));
+					
                     temp_val = temp_val + var;
-        		}
-        	}
-			result = (1.0 / (dim * dim)) * temp_val;
-			temp.setPix(rows, cols, result);
-			temp_val = 0;
+                }
+            }
+            result = (1.0 / (dim * dim)) * temp_val;
+            temp.setPix(rows, cols, result);
+            temp_val = 0;
         }
-	}
+    }
     return temp;
 }
 
@@ -399,39 +400,39 @@ Image Image::Sobel(){
 
 	for (int rows = 0; rows < sobelX.getRow(); rows++)
         for (int cols = 0; cols < sobelY.getCol(); cols++){
-        	sobelX.setPix(rows, cols, sobelx[rows][cols]);
-			sobelY.setPix(rows, cols, sobely[rows][cols]);
+            sobelX.setPix(rows, cols, sobelx[rows][cols]);
+		    sobelY.setPix(rows, cols, sobely[rows][cols]);
         }
 
 	for (int rows = 0; rows < this->getRow(); rows++)
         for (int cols = 0; cols < this->getCol(); cols++){
-        	for (int i = -1; i < 2; i++){
+            for (int i = -1; i < 2; i++){
                 for(int k = -1; k < 2; k++){
-        			if((rows + i < 0) || (cols + k < 0) || (rows + i > nrows - 1) || (cols + k > ncols - 1 )){
-        				var1 = 0;
-        				var2 = 0;
+        		    if((rows + i < 0) || (cols + k < 0) || (rows + i > nrows - 1) || (cols + k > ncols - 1 )){
+        			    var1 = 0;
+        			    var2 = 0;
                     }
-        			else{
-        				var1 = this->getPix(rows + i, cols + k) * sobelX.getPix(i + 1, k + 1);
-        				var2 = this->getPix(rows + i, cols + k) * sobelY.getPix(i + 1, k + 1);
+                    else{
+        			    var1 = this->getPix(rows + i, cols + k) * sobelX.getPix(i + 1, k + 1);
+        			    var2 = this->getPix(rows + i, cols + k) * sobelY.getPix(i + 1, k + 1);
                     }
 
                     temp_val1 = temp_val1 + var1;
                     temp_val2 = temp_val2 + var2;
-        		}
-        	}
+                }
+            }
 
-        	temp1.setPix(rows, cols, temp_val1);
+            temp1.setPix(rows, cols, temp_val1);
             temp2.setPix(rows, cols, temp_val2);
-        	temp_val1 = 0;
-        	temp_val2 = 0;
+            temp_val1 = 0;
+            temp_val2 = 0;
         }
 	// sqrt(Gx.^2 + Gy.^2) operation is being handled
     for (int rows = 0; rows < this->getRow(); rows++)
         for (int cols = 0; cols < this->getCol(); cols++)
-        	temp_ret.setPix(rows, cols, sqrt(pow(temp1.getPix(rows, cols), 2) + pow(temp2.getPix(rows, cols), 2)));
+            temp_ret.setPix(rows, cols, sqrt(pow(temp1.getPix(rows, cols), 2) + pow(temp2.getPix(rows, cols), 2)));
 
-	return temp_ret;
+    return temp_ret;
 }
 
 /* "HistogramEqualization" function has been generated to use in histogram equalization operations. This operation makes a continuous signal has a uniform
@@ -492,55 +493,56 @@ Image Image::FourierTransform(bool flag){
 	NewComplex complexImage[nrows * ncols];
 
 	// initializing complex image array
-	for(int rows = 0; rows < nrows; rows++)
-		for(int cols = 0; cols < ncols; cols++){
-			complexImage[rows * ncols + cols].real = 0;
-			complexImage[rows * ncols + cols].imag = 0;
-		}
+    for(int rows = 0; rows < nrows; rows++){
+        for(int cols = 0; cols < ncols; cols++){
+            complexImage[rows * ncols + cols].real = 0;
+            complexImage[rows * ncols + cols].imag = 0;
+	    }
+    }
+	
+    float result = 0;
+    float pi = 3.1419265;
+    float real = 0;
+    float imag = 0;
 
-	float result = 0;
-	float pi = 3.1419265;
-	float real = 0;
-	float imag = 0;
 
+    // 1-D fourier transform for rows
+    for(int rows = 0; rows < nrows; rows++){
+        for(int v = 0; v < ncols; v++){
+            for(int cols = 0; cols < ncols; cols++){
+			    real += this->getPix(rows, cols) * cos((2 * pi * v * cols) / ncols);
+			    imag += this->getPix(rows, cols) * -1 * sin((2 * pi * v * cols) / ncols);
+            }
+            complexImage[rows * ncols + v].real = real;
+            complexImage[rows * ncols + v].imag = imag;
+            real = 0;
+            imag = 0;
+        }
+    }
 
-	// 1-D fourier transform for rows
-	for(int rows = 0; rows < nrows; rows++){
-		for(int v = 0; v < ncols; v++){
-			for(int cols = 0; cols < ncols; cols++){
-				real += this->getPix(rows, cols) * cos((2 * pi * v * cols) / ncols);
-				imag += this->getPix(rows, cols) * -1 * sin((2 * pi * v * cols) / ncols);
-			}
-			complexImage[rows * ncols + v].real = real;
-			complexImage[rows * ncols + v].imag = imag;
-			real = 0;
-			imag = 0;
-		}
-	}
+    // then 1-D fourier transform for columns
+    for(int v = 0; v < ncols; v++){
+        for(int u = 0; u < nrows; u++){
+            for(int rows = 0; rows < nrows; rows++){
+                // there is a minus in the middle of the equation since j * j = -1 equality occurs there
+                real += complexImage[rows * ncols + u].real * cos((2* pi * u * rows) / nrows) - complexImage[rows * ncols + u].imag * -1 * sin((2* pi * u * rows) / nrows);
+                imag += complexImage[rows * ncols + u].real * -1 * sin((2* pi * u * rows) / nrows) + complexImage[rows * ncols + u].imag * cos((2* pi * u * rows) / nrows);
+            }
+            result = sqrt(pow(real,2) + pow(imag,2));
+            temp.setPix(u, v, result); // set new pixel values on temporal image object
+            real = 0;
+            imag = 0;
+        }
+    }
 
-	// then 1-D fourier transform for columns
-	for(int v = 0; v < ncols; v++){
-		for(int u = 0; u < nrows; u++){
-			for(int rows = 0; rows < nrows; rows++){
-				// there is a minus in the middle of the equation since j * j = -1 equality occurs there
-				real += complexImage[rows * ncols + u].real * cos((2* pi * u * rows) / nrows) - complexImage[rows * ncols + u].imag * -1 * sin((2* pi * u * rows) / nrows);
-				imag += complexImage[rows * ncols + u].real * -1 * sin((2* pi * u * rows) / nrows) + complexImage[rows * ncols + u].imag * cos((2* pi * u * rows) / nrows);
-			}
-			result = sqrt(pow(real,2) + pow(imag,2));
-			temp.setPix(u, v, result); // set new pixel values on temporal image object
-			real = 0;
-			imag = 0;
-		}
-	}
+    // if flag is true, logarithmic scaling will be applied on the spectrum
+    if(flag == true){
+        for(int rows = 0; rows < nrows; rows++)
+            for(int cols = 0; cols < ncols; cols++)
+                temp.setPix(rows, cols, log10(1 + abs(temp.getPix(rows, cols))));
+    }
 
-	// if flag is true, logarithmic scaling will be applied on the spectrum
-	if(flag == true){
-		for(int rows = 0; rows < nrows; rows++)
-			for(int cols = 0; cols < ncols; cols++)
-				temp.setPix(rows, cols, log10(1 + abs(temp.getPix(rows, cols))));
-	}
-
-	return temp;
+    return temp;
 }
 
 // "BasicThreshold" function performs a simple thresholding process on a given image.
@@ -639,7 +641,7 @@ Image Image::OtsuThreshold(){
                 temp.setPix(rows, cols, 0);
     }
 
-	return temp;
+    return temp;
 }
 
 // "uint2double" function takes an image which is in 'uint8' type and converts it to 'double'.
