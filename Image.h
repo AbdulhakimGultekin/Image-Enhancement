1e97148bb373c6825f0f6c5ac7a52aa8b9cedaef
AbdulhakimGultekin <hkmgltkn@gmail.com>
2018-04-20 21:19:48 +0300

    blanks arranged

--- Image.h
+++ Image.h
@@ -18,30 +18,30 @@ Instructor	:	Assist. Prof. A. Köksal Hocaoğlu
 class Image{
 	public:
 		// constructors and destructor
-		Image(int = 0, int = 0);									// default constructor
-		Image(const Image &);										// copy constructor
-		~Image();													// destructor
+		Image(int = 0, int = 0);                                    // default constructor
+		Image(const Image &);                                       // copy constructor
+		~Image();                                                   // destructor
 		// create image
-		void createImage(int, int);									// create an image, parameters all set
-		void initImage(float init = 0.0);							// initiate the pixel value of an image, the default is 0.0
+		void createImage(int, int);                                 // create an image, parameters all set
+		void initImage(float init = 0.0);                           // initiate the pixel value of an image, the default is 0.0
 		// get and set functions
-		int getRow() const;											// get row # / the height of the image
-		int getCol() const;											// get col # / the width of the image
-		float getMaximum() const;									// get the maximum pixel value
-		float getMinimum() const;									// get the mininum pixel value
-		float getPix(int rows, int cols);							// get pixel value at (rows, cols)
-		Image getImage() const;										// get the image
+		int getRow() const;                                         // get row # / the height of the image
+		int getCol() const;                                         // get col # / the width of the image
+		float getMaximum() const;                                   // get the maximum pixel value
+		float getMinimum() const;                                   // get the mininum pixel value
+		float getPix(int rows, int cols);                           // get pixel value at (rows, cols)
+		Image getImage() const;                                     // get the image
 
-		void setRow(int);											// set row number
-		void setCol(int);											// set column number
-		void setPix(int rows, int cols, float value);				// set pixel value at (rows, cols)
-		void setImage(Image &);										// set the image
+		void setRow(int);                                           // set row number
+		void setCol(int);                                           // set column number
+		void setPix(int rows, int cols, float value);               // set pixel value at (rows, cols)
+		void setImage(Image &);                                     // set the image
 		// operator overloading functions
-		float & operator()(int, int c = 0) const;					// operator overloading (i,j), when c = 0, a column vector
-		const Image operator=(const Image &);						// overloading = operator 
+		float & operator()(int, int c = 0) const;                   // operator overloading (i,j), when c = 0, a column vector
+		const Image operator=(const Image &);                       // overloading = operator 
 		// read and write functions
-		void readImage(char *fname);								// read the PGM format image file
-		void writeImage(char *fname, bool flag = false);			// write the PGM image to the file
+		void readImage(char *fname);                                // read the PGM format image file
+		void writeImage(char *fname, bool flag = false);            // write the PGM image to the file
 	
 		// MEMBER FUNCTIONS FOR IMAGE ENHANCEMENT
 		Image thresholdImage(float threshold = 127.0, float lowValue = 0.0, float highValue = 255.0);
@@ -52,15 +52,15 @@ class Image{
 		Image HistogramEqualization();
 		Image FourierTransform(bool flag = true);
 		Image BasicThreshold();
-		Image OtsuThreshold();										// will be reviewed
+		Image OtsuThreshold();                                      // will be reviewed
 		Image uint2double();
 		Image HoughImage();
 		// END OF MEMBER FUNCTIONS
 	private:
-		int nrows;													// number of rows / height
-		int ncols;													// number of columns / width
-		int maximum;												// the maximum pixel value
-		float *image;												// image buffer
+		int nrows;                                                  // number of rows / height
+		int ncols;                                                  // number of columns / width
+		int maximum;                                                // the maximum pixel value
+		float *image;                                               // image buffer
 };
 
 #endif
